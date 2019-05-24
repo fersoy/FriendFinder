419d6968cd5b382633f174fb545916cea54a9e45
Fatih Ersoy <f.ersoytx@gmail.com>
2019-05-24 04:52:28 -0500

    select tag has been fixed. Server.js has been created

---
+++ server.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+    {
+        name: 'Ronaldo',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/220px-Cristiano_Ronaldo_2018.jpg',
+        scores: [
+            '1',
+            '1',
+            '2',
+            '3',
+            '2',
+            '3',
+            '2',
+            '3',
+            '2',
+            '3'
+        ]
+    },
+    {
+        name: 'Messi',
+        photo: 'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/05/22/15585327368268.jpg',
+        scores: [
+            '5',
+            '5',
+            '3',
+            '4',
+            '4',
+            '5',
+            '2',
+            '3',
+            '2',
+            '4'
+        ]
+    },
+    {
+        name: 'Salah',
+        photo: 'https://futhead.cursecdn.com/static/img/19/players_alt/p184758707.png',
+        scores: [
+            '3',
+            '1',
+            '3',
+            '2',
+            '5',
+            '4',
+            '2',
+            '2',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Mbappe',
+        photo: 'http://assets.rappler.com/612F469A6EA84F6BAE882D2B94A4B421/img/D86A00EC90F14CA7A4594D1C7C6D81AB/kylian-mbappe.jpg',
+        scores: [
+            '3',
+            '4',
+            '5',
+            '1',
+            '3',
+            '4',
+            '2',
+            '4',
+            '5',
+            '2'
+        ]
+    },
+    {
+        name: 'Bale',
+        photo: 'https://www.101greatgoals.com/wp-content/uploads/2019/02/GettyImages-1054743218-910x607.jpg',
+        scores: [
+            '5',
+            '4',
+            '2',
+            '3',
+            '1',
+            '4',
+            '3',
+            '1',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Mane',
+        photo: 'http://images.sportspromedia.com/images/made/images/uploads/news/Sadio_Mane_LFC_600_424.jpg',
+        scores: [
+            '2',
+            '2',
+            '3',
+            '4',
+            '5',
+            '3',
+            '2',
+            '4',
+            '2',
+            '1'
+        ]
+    },
+    {
+        name: 'Neymar',
+        photo: 'https://cdn.images.dailystar.co.uk/dynamic/58/photos/190000/620x/Neymar-683840.jpg',
+        scores: [
+            '1',
+            '5',
+            '3',
+            '2',
+            '5',
+            '5',
+            '4',
+            '3',
+            '4',
+            '4'
+        ]
+    },
+    {
+        name: 'Eden Hazard',
+        photo: 'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2019/04/17/15555251330175.jpg',
+        scores: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '2',
+            '3',
+            '2',
+            '2',
+            '1'
+        ]
+    },
+    {
+        name: 'Benzema',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/CSKA-RM18_%2811%29.jpg/220px-CSKA-RM18_%2811%29.jpg',
+        scores: [
+            '4',
+            '3',
+            '2',
+            '2',
+            '1',
+            '5',
+            '4',
+            '3',
+            '4',
+            '5'
+        ]
+    },
+    {
+        name: 'Sergio Ramos',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Russia-Spain_2017_%286%29.jpg/230px-Russia-Spain_2017_%286%29.jpg',
+        scores: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '5',
+            '4',
+            '3',
+            '2',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
